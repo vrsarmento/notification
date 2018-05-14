b604f8606ecdc2b9174d65c29856f9dc506e8e0c
Victor Rocha <vrsarmento@gmail.com>
2018-05-14 20:40:05 -0300

    Updates in /test

--- test/index.php
+++ test/index.php
@@ -3,5 +3,5 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2, "mail.gmail.com", "test@gmail.com", "test@123", "tls", "587", "test@gmail.com", "Teste");
-$novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>teste</b>!</p>", "vrsarmento@gmail.com", "Victor Rocha", "vrsarmento@hotmail.com", "Victor Sarmento");
+$newEmail = new Email(2, "host.email.com", "test@email.com", "password", "tls/ssl", "Port", "test@email.com", "From Name");
+$newEmail->sendMail("Subject", "E-mail body", "test@email.com", "Reply Name", "test@email.com", "Address Name");
